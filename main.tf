d748bd95614a82f34a920754b532b379101af8a6
SANTOSH KUMAR MYAKALA <54053423+msantoshnetha@users.noreply.github.com>
2021-05-02 20:37:06 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws"{
+	region = "eu-central-1"
+	access_key = "AKIA5ETYEZAEFBYLIPJO"
+	secret_key = "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"
+}
+
+resource "aws_instance" "ec2"{
+	ami = "ami-0767046d1677be5a0"
+	insatnce_type = "t2.micro"
+	key_name = "ansible_frkf_key"
+	security_groups = ["ansible-SG"]
+	tags = {
+		Name = "Ansible"
+	}
+	
+	provisioner "local-exec" {
+		command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --private-key /home/usant/ansible_frkf_key.pem -i ${aws_instance.ec2.public_ip}, playbook.yaml"
+	}
+}
+
